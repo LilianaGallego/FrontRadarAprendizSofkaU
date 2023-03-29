af0f9e78ce35d54a185f42a6c877b589976aa91d
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-03-28 22:03:12 -0500

    implementacion de firebaseauth y su servicio

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-fire-authh',
+    appId: '1:985774395894:web:c1b8e2f45ca0d27a8b37d7',
+    storageBucket: 'login-fire-authh.appspot.com',
+    apiKey: 'AIzaSyC55CXCWC-qih6bzsTGFJN-_OryGkFWsR8',
+    authDomain: 'login-fire-authh.firebaseapp.com',
+    messagingSenderId: '985774395894',
+  },
+      production: false
+}
