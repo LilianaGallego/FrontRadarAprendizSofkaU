101135eeb83f1030b225dc37837d90d438560459
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-03-29 11:50:21 -0500

    implementacion de toastr en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
   ],
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthfireService } from 'src/app/services/authfire.service';
 
 @Component({
@@ -15,7 +16,7 @@ export class LoginComponent {
 
   constructor(private auth: AuthfireService,
     private router: Router,
-    ){
+    private toast: ToastrService){
 
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
@@ -24,6 +25,7 @@ export class LoginComponent {
   }
 
   async login(){
+
     const email = this.form.value.email;
     const password = this.form.value.password;
     console.log(this.form.value);
@@ -31,21 +33,26 @@ export class LoginComponent {
     this.auth.login(this.form.value)
     .then(res => {
       console.log(res);
+      this.toast.success('Logeado correctamente...', 'Redireccionando.')
       this.router.navigate(['/website']);
     })
-    .catch(err => console.log(err))
+    .catch(err =>{ 
+      this.toast.error('Rectifique los datos...', 'Contraseña o correo incorrecto.')
+      console.log(err)
+    })
 
   }
+
 
   async onReset(){
     try{
     const email = this.emailpass.value;
     
    await this.auth.resetPassword(email!);
+   this.toast.success('Correo enviado...', 'Revise porfavor su bandeja de entrada')
 
-    window.alert('se envio.');
     }catch(err){console.log(err);
-  
+      this.toast.error("Correo incorrecto.","Rectifique los datos")
     }
 
   }
