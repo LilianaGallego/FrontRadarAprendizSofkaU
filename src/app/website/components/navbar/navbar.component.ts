012a8da4d9f6bcc88d60628bdc8202e0a024caff
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-03-29 17:03:03 -0500

    creacion de servicios
    se crean los servicios para consumir el back del negocio, quedan faltando algunos por agregar. Tambien se implementa el manejo a la hora del login con el rol y se crea el boton de logout funcionando completamente

--- src/app/website/components/navbar/navbar.component.ts
+++ src/app/website/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  constructor(private router: Router, private toast: ToastrService) { }
+  ngOnInit(): void {
+
+  }
+
+  logout() {
+    localStorage.clear();
+
+    this.toast.success('Sesión cerrada.', 'Redireccionado al login...')
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 2000);
+  }
 }
---
+++ src/shared/models/knowledgeArea.ts
@@ -0,0 +1,9 @@
+export interface KnowledgeArea{
+      name:string;
+      descriptor:string;
+      factual:number;
+      conceptual:number;
+      procedural:number;
+      metacognitive:number;
+      appropriationLevel:number;
+}
