add5feccd9d106f700db5d9ab8149e63b761e7b1
Mike Arango <maicol@Macbooks-MacBook-Pro.local>
2023-03-29 21:12:03 -0500

    se implementa el crear usuario
    queda funcionando correctamente consumiendo firebase y consumiendo el back en mongo, falta realizar validaciones front

--- src/app/website/pages/create-user/create-user.component.ts
+++ src/app/website/pages/create-user/create-user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthfireService } from 'src/app/services/authfire.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/shared/models/user';
 
 @Component({
   selector: 'app-create-user',
@@ -9,32 +14,79 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class CreateUserComponent {
 
   disableSelect = new FormControl(false);
+  regForm!: FormGroup;
 
-  roles: Roles[]= [
-    {value: "deafult", viewValue: 'Seleccione una opción'},
-    {value: "apprendice", viewValue: 'Aprendiz'},
-    {value: "operator", viewValue: 'Operador'},
-    {value: "admin", viewValue: 'Administrador'},
+  constructor(private toast: ToastrService,
+    private auth: AuthfireService,
+    private fb: FormBuilder,
+    private route: Router,
+    private userService: UserService) {
+      this.crearFormulario()
+     }
+
+  roles: Roles[] = [
+    { value: "deafult", viewValue: 'Seleccione una opción' },
+    { value: "ROLE_APPRENTICE", viewValue: 'Aprendiz' },
+    { value: "ROLE_OPERATION", viewValue: 'Operador' },
+    { value: "ROLE_ADMIN", viewValue: 'Administrador' },
   ];
 
-  regForm = new FormGroup({
-    username: new FormControl('', Validators.required),
-    lastname: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    rol: new FormControl(this.roles[0].value, Validators.required)
-  })
-
-  onReg(){
-    console.log(this.regForm.value);
+  crearFormulario(): void {
+    this.regForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      password1: ['', [Validators.required, Validators.minLength(4)]],
+      role: [this.roles[0].value, Validators.required]
+    })
   }
 
-}
 
+  onReg() {
+
+    if (this.regForm.invalid) {
+
+      this.toast.error('Intenta de nuevo', 'Error en los datos!');
+      return Object.values(this.regForm.controls).forEach(control => {
+        control.markAllAsTouched();
+      })
+    } else {
+      var fireForm = {
+        email: this.regForm.value.email,
+        password: this.regForm.value.password
+      }
+      try{
+      return this.auth.register(fireForm).then(res => {
+        var body = {
+          name: this.regForm.value.name,
+          lastName: this.regForm.value.lastName,
+          email: this.regForm.value.email,
+          password: res.user.providerId,
+          role: this.regForm.value.role
+        }
+        this.userService.saveUser(body).subscribe(res => {
+
+          this.toast.success('Redirigiendo...', 'Registro exitoso!')
+          setTimeout(() => {
+            this.route.navigate(['/website/user'])
+          }, 1500);
+        })
+
+      })
+    }catch(err){
+      console.log(err);
+     return this.toast.error('Error inesperado', 'Vuelve a intentarlo, porfavor.');
+    }
+    }
+
+  }
+}
 interface Roles {
   value: string;
   viewValue: string;
 }
+
 
 // enum Role {
 //   ROLE_APPRENDICE = "apprendice",
